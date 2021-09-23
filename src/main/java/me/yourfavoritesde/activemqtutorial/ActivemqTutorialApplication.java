5bee50fd49d41e5ef8bd281fe018bb1ab1e35f7d
Kaliham <thekaliham@gmail.com>
2021-09-23 20:39:41 +0300

    initial commit

---
+++ src/main/java/me/yourfavoritesde/activemqtutorial/ActivemqTutorialApplication.java
@@ -0,0 +1,13 @@
+package me.yourfavoritesde.activemqtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yourfavoritesde/activemqtutorial/ActivemqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yourfavoritesde.activemqtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
